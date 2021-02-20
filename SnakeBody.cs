551d60f118352b8fab56d2e347e54821ee9f81a2
skibicastle <59386192+skibicastle@users.noreply.github.com>
2021-02-20 18:55:40 +0300

    Add files via upload

--- SnakeBody.cs
+++ SnakeBody.cs
@@ -15,44 +15,55 @@ public class SnakeBody : MonoBehaviour
     {
         startLenght = food.GetComponent<SpawnPoint>().score;
         positions.Add(head.position);
-        InstantiateBody();
-        InstantiateBody();
-        StepAlwaysMove = AlwaysMove(1f);
-        StartCoroutine(StepAlwaysMove);
+        //InstantiateBody();
+        //InstantiateBody();
     }
 
     // Update is called once per frame
     void Update()
     {
+        const int bodySpawnNum = 10;
         bodyLenght = food.GetComponent<SpawnPoint>().score;
-        bodyLenght /= 10;
+        bodyLenght /= bodySpawnNum;
         if (bodyLenght != startLenght)
         {
             InstantiateBody();
             startLenght = bodyLenght;
-        }    
+        }
+
+        SnakeMove();
     }
-    private IEnumerator AlwaysMove(float WaitTime)
+    /*private IEnumerator AlwaysMove(float WaitTime)
     {
         while (true)
         {
-            float distance = ((Vector2)head.position - positions[0]).magnitude;
-            if (distance > 0.5f)
-            {
-                positions.Insert(0, head.position);
-                positions.RemoveAt(positions.Count - 1);
-            }
 
-            for (int i = 0; i < bodySnake.Count; i++)
-            {
-                bodySnake[i].position = Vector2.Lerp(positions[i + 1], positions[i], distance/1);
-            }
             yield return new WaitForSeconds(0);
+        }
+    }*/
+    void SnakeMove()
+    {
+        float distance = ((Vector2)head.position - positions[0]).magnitude;
+        float diameterSquare = 1f; //diameter of the inscribed circle
+        if (distance > diameterSquare)
+        {
+            Vector2 directionBeforeHeadToNewPosition = ((Vector2)head.position - positions[0]);
+
+            positions.Insert(0, positions[0] + directionBeforeHeadToNewPosition);
+            positions.RemoveAt(positions.Count - 1);
+
+            distance -= diameterSquare;
+        }
+
+        for (int i = 0; i < bodySnake.Count; i++)
+        {
+            bodySnake[i].position = Vector2.Lerp(positions[i + 1], positions[i], distance / diameterSquare);
+            Debug.Log($" переменная x ={bodySnake[i].position.x} переменная y ={bodySnake[i].position.y}");
         }
     }
     void InstantiateBody()
     {
-        Transform body = Instantiate(head,positions[positions.Count-1],Quaternion.identity, transform);
+        Transform body = Instantiate(head,positions[positions.Count-1],Quaternion.identity,transform);
         bodySnake.Add(body);
         positions.Add(body.position);
     }  
